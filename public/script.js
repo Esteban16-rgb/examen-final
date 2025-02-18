9a70691a3bc1a0a54210c5405574afefd9e5679a
Esteban16-rgb <markestebangg@gmail.com>
2025-02-18 11:49:40 +0100

    jjjjjjjjj

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ document.getElementById("registerForm").addEventListener("submit", async (event)
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("http://localhost:3000/register", {
+    const response = await fetch("/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
